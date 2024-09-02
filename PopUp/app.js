7d65161e1afebeefa23ea32108558b930f843961
Matheus Ursino dos Santos <66560955+MatheusUrsino@users.noreply.github.com>
2024-09-02 20:39:30 -0300

    Add files via upload

---
+++ PopUp/app.js
@@ -0,0 +1,11 @@
+let popup = document.getElementById("popup");
+
+function openPopUp()
+{
+    popup.classList.add("open-popup");
+}
+
+function ClosePopUp()
+{
+    popup.classList.remove("open-popup");
+}
